5640ff3e481310d68f1079565851e2381193582d
Robert Forest <67814666+rvforest@users.noreply.github.com>
2025-05-27 20:14:14 -0400

    Update nvim install

--- setup-aarch.sh
+++ setup-aarch.sh
@@ -18,7 +18,10 @@ git config --global pull.rebase true
 
 # neovim
 echo "installing neovim..."
-sudo apt install -y neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.zshrc
 
 # zsh
 echo "installing zsh..."
