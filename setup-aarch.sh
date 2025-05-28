f125416050d40c0250b38fd0f34190c94d3212cc
Robert Forest <67814666+rvforest@users.noreply.github.com>
2025-05-27 21:03:25 -0400

    Fix nvim paths

--- setup-aarch.sh
+++ setup-aarch.sh
@@ -20,8 +20,8 @@ git config --global pull.rebase true
 echo "installing neovim..."
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.zshrc
+sudo tar -C /opt -xzf nvim-linux-arm64tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux-arm64/bin"' >> ~/.zshrc
 
 # zsh
 echo "installing zsh..."
